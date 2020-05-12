4161cc0961398c58672e89bbddcde5a933c3acdf
EC2 Default User <ec2-user@ip-172-31-32-35.us-east-2.compute.internal>
2020-05-12 14:08:08 +0000

    Laravel QUEST001

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+                'name' => 'sample1',
+                'email' => 'sample1@sample.com',
+                'password' => bcrypt('sample1')
+            ]);
+        DB::table('users')->insert([
+                'name' => 'sample2',
+                'email' => 'sample2@sample.com',
+                'password' => bcrypt('sample1')
+            ]);
+        DB::table('users')->insert([
+                'name' => 'sample3',
+                'email' => 'sample3@sample.com',
+                'password' => bcrypt('sample1')
+            ]);
+        DB::table('users')->insert([
+                'name' => 'sample4',
+                'email' => 'sample4@sample.com',
+                'password' => bcrypt('sample1')
+            ]);
+        DB::table('users')->insert([
+                'name' => 'sample5',
+                'email' => 'sample5@sample.com',
+                'password' => bcrypt('sample1')
+            ]);
+
+    }
+}
